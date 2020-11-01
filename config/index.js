6a9568e0f18e58d4985d7f0d03df8e1e50143b08
fatma <fatmalabidi1.1119@gmail.com>
2020-11-01 19:49:11 +0100

    fix config

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
- export default {
-     // will be used to connect to mongoDB (mlab) via mongoose
-     getWorkflowDb: function(config) {
-         return `mongodb://${config.mongo.userName}:${config.mongo.pwd}@ds235169.mlab.com:35169/workflow`
-     },
- }
+import config from './config.js'
+
+export default {
+    // will be used to connect to mongoDB (mlab) via mongoose
+    getWorkflowDb: function() {
+        return `mongodb://${config.mongo.userName}:${config.mongo.pwd}@ds235169.mlab.com:35169/workflow`
+    },
+}
