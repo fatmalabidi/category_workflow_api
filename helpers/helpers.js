1e8489d52b496973051212a1c312d1f06b52b119
fatma <fatmalabidi1.1119@gmail.com>
2020-11-01 17:30:59 +0100

    add helpers_test & helpers

---
+++ helpers/helpers.js
@@ -0,0 +1,5 @@
+export default {
+    IsValidStatus: function(status) {
+        return status > 0 && status <= 5
+    }
+}
---
+++ test/helpers_test.js
@@ -0,0 +1,21 @@
+import assert from "assert"
+import helpers from "../helpers/helpers.js"
+
+// TODO add IsValidStatus test
+describe('IsValidStatus', function() {
+    describe('valid status', function() {
+        it('should return true  when the status is less oe equal than 5', function() {
+            assert.equal(helpers.IsValidStatus(1), true);
+        });
+    });
+    describe('invalid status 6', function() {
+        it('should return true  when the status is less oe equal than 5', function() {
+            assert.equal(helpers.IsValidStatus(6), false);
+        });
+    });
+    describe('invalid status -1', function() {
+        it('should return true  when the status is less oe equal than 5', function() {
+            assert.equal(helpers.IsValidStatus(-1), false);
+        });
+    });
+});
